67dec8fd9f9ff10a340f44c440b44475d0c1f5d5
dimastepanov <dimastepanov1980@gmail.com>
2024-06-27 11:52:03 +0700

    Chane send_to_crm method

--- crm.py
+++ crm.py
@@ -2,7 +2,7 @@ import logging
 import httpx
 
 async def send_to_crm(data):
-    crm_url = f"https://bike-crm-7f78192cffc8.herokuapp.com/webhook"  # Установите URL CRM в пустую строку для тестирования
+    crm_url = "https://bike-crm-7f78192cffc8.herokuapp.com/webhook"  # Установите правильный URL вашего CRM
     if not crm_url:
         logging.info(f"CRM URL is empty. Data to send: {data}")
         return
@@ -11,5 +11,6 @@ async def send_to_crm(data):
         try:
             response = await client.post(crm_url, json=data)
             response.raise_for_status()
+            logging.info(f"Data sent to CRM: {data}")
         except httpx.HTTPStatusError as e:
             logging.error(f"Error sending to CRM: {e.response.text}")
