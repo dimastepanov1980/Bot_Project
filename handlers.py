2c2a917854012d1c5fb080b30439b1980fd255b9
dimastepanov <dimastepanov1980@gmail.com>
2024-06-27 12:10:37 +0700

    Change send to crm method

--- handlers.py
+++ handlers.py
@@ -18,14 +18,17 @@ async def start(update: Update, context: CallbackContext) -> None:
 async def handle_message(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text
     chat_id = update.message.chat_id
-    await send_to_crm({'message': user_message, 'type': 'user'})
+    user_id = update.message.from_user.id  # Получение ID пользователя
+    await send_to_crm({'message': user_message, 'type': 'user', 'user_id': user_id})
+
     logger.info(f"Получено сообщение: {user_message}")
 
     try:
         ai_reply = await get_assistant_response(user_message, chat_id)
         logger.info(f"Ответ OpenAI: {ai_reply}")
         await update.message.reply_text(ai_reply)
-        await send_to_crm({'message': ai_reply, 'type': 'bot'})
+        await send_to_crm({'message': ai_reply, 'type': 'bot', 'user_id': user_id})
     except Exception as e:
         logger.error(f"Ошибка при получении ответа от OpenAI: {e}")
-        await update.message.reply_text('Произошла ошибка при обработке вашего запроса.')
+        await update.message.reply_text('Произошла ошибка при обработке вашего запроса!')
+    
